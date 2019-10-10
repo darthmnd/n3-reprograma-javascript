d82119a1dedfe14ead3e34e1089dbf83676a0b74
Amanda Andrade <47823398+darthmnd@users.noreply.github.com>
2019-10-09 22:27:48 -0300

    Desafio Inacabado

--- exercíciosSemana06.js
+++ exercíciosSemana06.js
@@ -54,15 +54,44 @@
 
 // Escreva uma função que receba uma array como parâmetro e devolva outra, mas sem elementos repetidos.
 
-const arr = [ 45, 765, 23, 76, 43, 65, 2345, 677, 5, 34, 22, 546, 77, 4355, 5667, 45, 56, 7, 4, 755, 45]
-function numerosVarios(array) {
-    for(let i = 0; i < array.length; i++){
-        console.log('loop i', array[i])
-        for(let j = 1; j < array.length; j++){
-            console.log('loop j'), array[j]
-            
-        }
-        
-    } 
-}
-console.log(numerosVarios(arr))
+// function removeValue(arr) {
+//     const newArr= []
+//     for(let i = 0; i < arr.length; i++){
+//         for(let j = 1; j < arr.length; j++){
+          
+//             if (arr.indexOf(arr[i]) != (newArr.indexOf(arr[j]))){
+//                 newArr.push(arr)
+//             }
+//         } 
+//         return newArr;
+//     } 
+// }
+// console.log([ 45, 765, 23, 76, 43, 65, 2345, 677, 5, 34, 22, 546, 77, 4355, 5667, 45, 56, 7, 4, 755, 45]
+
+
+/* VALIDAÇÃO DE CARTÃO DE CRÉDITO (ALGORITMO DE LUHN)
+Nesse exercício, você fará a validação de números de cartão de crédito utilizando o algoritmo de Luhn:
+1) Retire o último dígito do número. Ele é o verificador;
+2) Escreva os números na ordem inversa;
+3) Multiplique os dígitos das casas ímpares por 2 e subtraia 9 de todos os resultados maiores que 9;
+4) Some todos os números;
+5) O dígito verificador (aquele do passo 1) é o número que você precisa somar a todos os outros números somados pra obter um módulo 10.
+6) O cartão será válido se os números somados % 10 === 0.
+*/
+
+////////////////////////////
+
+// let cartao = '4532841148586221'
+// let resultado = cartao.split('')
+// let ultimoDigito = resultado.pop()
+//     resultado.reverse()
+//     resultado.forEach(function digitosImpares(digitos){
+//         let i = 0;
+//         if (i % 2 === 1){
+//             resultado[i] *= 2
+//             return resultado
+//         }
+//         console.log(resultado)
+//     })
+
+
