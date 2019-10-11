001db03978a7d2cb980e63e04fe2a02864d12bea
Amanda Andrade <47823398+darthmnd@users.noreply.github.com>
2019-10-10 22:15:37 -0300

    exercício não finalizado

---
+++ objetos.js
@@ -0,0 +1,22 @@
+// imprimir lista de cores por gato, no formato
+// nome do gato
+//cor
+//-------------
+
+// const navi = {
+//     nome: 'Navi',
+//     cores: ['marrom', 'branca']
+//   };
+  
+//   const quico = {
+//     nome: 'Quico',
+//     cores: ['branco','laranja']
+//   };
+  
+//   const chiquinha = {
+//     nome: 'Chiquinha',
+//     cores: ['tigrada']
+//   };
+
+// for(let caracteristica in navi)
+//   console.log(caracteristica, navi[caracteristica])
